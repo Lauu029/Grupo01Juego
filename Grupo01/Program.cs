8dbab437b2d51cefc0f7de7cedebb9d9e4432937
Laura Gomez <lgomez25@ucm.es>
2021-04-12 12:05:43 +0200

    Archivos iniciales añadidos al indice del repositorio

---
+++ Grupo01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grupo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
